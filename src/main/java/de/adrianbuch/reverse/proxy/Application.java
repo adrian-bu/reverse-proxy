fd496e03478e77a40e1e253f9b0eb8bb804ab0e9
Adrian Buch <privat@adrian-buch.de>
2021-05-30 15:56:06 +0200

    Generated some sample data

--- src/main/java/de/adrianbuch/reverse/proxy/Application.java
+++ src/main/java/de/adrianbuch/reverse/proxy/Application.java
@@ -1,13 +1,38 @@
 package de.adrianbuch.reverse.proxy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.adrianbuch.reverse.proxy.domain.request.HTTPRequest;
+import de.adrianbuch.reverse.proxy.repository.HTTPRequestRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	private final HTTPRequestRepository httpRequestRepository;
+
+	@Autowired
+	public Application(HTTPRequestRepository httpRequestRepository) {
+		this.httpRequestRepository = httpRequestRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		/*
+		Generates some sample data
+		*/
+		for (var i = 0; i < 7; i++) {
+			HTTPRequest httpRequest = new HTTPRequest();
+			httpRequest.setUsername("Username: " + i);
+			httpRequest.setIsMalicious(Boolean.TRUE);
+			httpRequestRepository.save(httpRequest);
+		}
+	}
 }
